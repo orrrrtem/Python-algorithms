fab2617305d15a6d63ca63bf65e62482da3d8c94
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2020-04-25 22:49:22 +0300

    measurements for string searching

---
+++ tools/string_searching/make_measurements.py
@@ -0,0 +1,46 @@
+import time
+import pandas as pd
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+parentdir2 = os.path.dirname(parentdir)
+sys.path.insert(0,parentdir)
+sys.path.insert(0,parentdir2)
+from time_metrics import *
+from algorithms.string_searching.naive import naive
+from algorithms.string_searching.robin_carp import robin_carp
+from algorithms.string_searching.kmp import kmp
+from algorithms.string_searching.bmh import 
+
+def get_time_df(num_iterations = 10, methods = [naive], len_cases = 4 ):
+    time_total = pd.DataFrame(columns=['file', 'method', 'mean', 'median', 'min', 'max', 'variance'])
+    df_index = 0
+    for i in range(0, len_cases):
+        for method in methods:
+            time_good = []
+            time_bad = []
+            for j in range(0, num_iterations):
+                t0 = time.time()
+                method(good_patterns[i], good_texts[i])
+                t1 = time.time()
+                total = t1-t0
+                time_good.append(total)
+
+                t0 = time.time()
+                method(bad_patterns[i], bad_texts[i])
+                t1 = time.time()
+                total = t1-t0
+                time_bad.append(total)
+            time_good.sort()
+            time_bad.sort()
+            time_total.loc[df_index] = [good_files[i], str(method.__name__), get_mean(time_good),
+                                         get_median(time_good), get_min(time_good), get_max(time_good), get_variance(time_good)]
+            time_total.loc[df_index + 1] = [bad_files[i], str(method.__name__), get_mean(time_bad),
+                                             get_median(time_bad), get_min(time_bad), get_max(time_bad), get_variance(time_bad)]
+            df_index += 2
+    return time_total
+
+all_methods = [naive, robin_carp, kmp, bmh]
+time_df = get_time_df(num_iterations=1000, methods = all_methods)
+time_df.to_csv('measurements')
