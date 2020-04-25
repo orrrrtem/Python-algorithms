67f7761293e070938495c6b874d6313b05b968fb
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2020-04-25 22:41:03 +0300

    delete try except methods

--- algorithms/string_searching/bmh.py
+++ algorithms/string_searching/bmh.py
@@ -1,28 +1,22 @@
 from copy import deepcopy
 
 def get_int_image(x):
-    image_dict = {}
     length = len(x)
+    image_dict = {i: length for i in range(2048)}
     for idx, char in enumerate(x[:-1], start=1):
         # if char not in image_dict:
-        image_dict[char] = length-idx
-
-    if x[-1] not in image_dict:
-        image_dict[x[-1]] = length
-
-    image_dict['other'] = length
+        image_dict[ord(char)] = length-idx
 
     return image_dict
 
 
 def bmh(pattern, text):
-# Boyer–Moore–Horspool algorithm
     entries = []
     if len(pattern) > len(text):
         return entries
     image_dict = get_int_image(pattern)
     length_text = len(text)
-    length_pattern = image_dict['other']
+    length_pattern = len(pattern)
     i = length_pattern  # for text
     j = length_pattern  # for pattern
     entry_point = length_pattern  # for position in full string
@@ -33,10 +27,8 @@ def bmh(pattern, text):
         else:
             if j == 0:
                 entries.append(entry_point - length_pattern)
-            try:
-                entry_point += image_dict[text[i-1]]
-            except:
-                entry_point += image_dict['other']
+            # print(text[i-1])
+            entry_point += image_dict[ord(text[i-1])]
             i = deepcopy(entry_point)
             j = length_pattern
     return entries
