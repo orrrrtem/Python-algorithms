7b670c5277b4fcfff58657a93115165d3f57be3e
Artem Aroslankin <43597601+orrrrtem@users.noreply.github.com>
2020-04-25 18:29:48 +0300

    added test for string searching algorithms

---
+++ tests/test_string_searching.py
@@ -0,0 +1,18 @@
+from algoritms.string_searching.naive import naive
+from algoritms.string_searching.robin_carp import robin_carp
+from algoritms.string_searching.kmp import kmp
+from algoritms.string_searching.bmh import bmh
+
+text_test = 'ohoh;oh Fuck, oh, oh fucj, Fuck Fuuuuck   shiiit oh'
+pattern_test = 'oh'
+answer = [0, 2, 5, 14, 18, 49]
+
+methods = [naive, robin_carp, kmp, bmh]
+
+for method in methods:
+    if  answer  == method(pattern_test,text_test):
+          print(str(method.__name__),' pased')
+    else:
+          print(str(method.__name__),' failed')
+          print(str(method.__name__),' return ', method(pattern_test,text_test) , ' instead of ', answer)
+
